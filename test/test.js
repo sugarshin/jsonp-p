4710e1d27a98ec9d8e9470b8a0f9dc1ab9fe1fad
Shingo Sato <shinsugar@gmail.com>
2016-10-10 01:30:40 +0900

    Refactoring

--- test/test.js
+++ test/test.js
@@ -1,25 +1,16 @@
-import { jsdom } from 'jsdom';
-import assert from 'power-assert';
+/* eslint-disable max-nested-callbacks */
 
-import jsonpP from '..';
-
-global.document = jsdom('<html><head></head><body></body></html>');
-global.window = document.defaultView;
-global.navigator = window.navigator;
+import assert from 'assert';
+import jsonpP from '../src/jsonp-p';
 
 describe('jsonpP', () => {
-
   describe('200', () => {
-    it('case 1', () => {
+    it('ok', () => {
       const expected = {
         jsonpP: 'ok'
       };
       return jsonpP('http://www.mocky.io/v2/5611517d1200002c23953036')
-        .then(res => {
-          assert.deepEqual(res, expected, 'case 1');
-        })
-        .catch(err => console.log(err));
+        .then(res => assert.deepEqual(res, expected, 'ok'));
     });
   });
-
 });
