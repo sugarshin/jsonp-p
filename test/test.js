06e183e77d467b06504daf2d1de3ede8b54db4f1
Shingo Sato <shinsugar@gmail.com>
2016-10-23 01:33:04 +0900

    Can cancel

--- test/test.js
+++ test/test.js
@@ -1,16 +1,18 @@
-/* eslint-disable max-nested-callbacks */
-
 import assert from 'assert';
-import jsonpP from '../src/jsonp-p';
+import jsonpP from '../src';
 
 describe('jsonpP', () => {
-  describe('200', () => {
-    it('ok', () => {
-      const expected = {
-        jsonpP: 'ok'
-      };
-      return jsonpP('http://www.mocky.io/v2/5611517d1200002c23953036')
-        .then(res => assert.deepEqual(res, expected, 'ok'));
-    });
+  it('OK', () => {
+    const expected = {
+      jsonpP: 'ok'
+    };
+    return jsonpP('http://www.mocky.io/v2/5611517d1200002c23953036').promise
+      .then(res => assert.deepEqual(res, expected, 'ok'));
+  });
+  it('Cancel', () => {
+    const { cancel, promise } = jsonpP('http://www.mocky.io/v2/5611517d1200002c23953036');
+    assert(typeof cancel, 'function');
+    cancel();
+    return promise.catch(error => assert(error instanceof Error));
   });
 });
